7f5403a7aea1d92ebf7b874d527887a76ffe15fa
abhishek <abhiskg20@gmail.com>
2022-12-09 16:42:27 +0600

    Axios interceptor added

---
+++ frontend/src/utils/axios.utils.ts
@@ -0,0 +1,40 @@
+import axios, { AxiosResponse } from "axios";
+
+const client = axios.create({ baseURL: `http://localhost:5000/` });
+
+export const request = ({ ...options }) => {
+  client.defaults.headers.common.Authorization = `Bearer ${localStorage.getItem(
+    "address-book"
+  )}`;
+
+  const onSuccess = (response: AxiosResponse) => {
+    if (response.status === 200 && response.data.success) {
+      return response.data.data;
+    }
+    return response;
+  };
+  const onError = (error: any) => {
+    console.log(error);
+    if (
+      error.response?.status === 403 &&
+      error.response?.data?.success === false
+    ) {
+      throw new Error(error.response?.data?.message);
+    }
+    if (
+      error.response?.status === 401 &&
+      error.response?.data?.success === false
+    ) {
+      throw new Error(error.response?.data?.message);
+    }
+    if (
+      error.response?.status === 404 &&
+      error.response?.data?.success === false
+    ) {
+      throw new Error(error.response?.data?.message);
+    }
+
+    return error;
+  };
+  return client(options).then(onSuccess).catch(onError);
+};
